1f923b35feaeabb8e6a680d7e47f4e11527720d4
Jiaqi Wang <106310042+Eight-Winged-Angel@users.noreply.github.com>
2023-11-22 18:48:57 +0800

    Add files via upload

---
+++ back-end/src/test/java/cn/edu/cupk/learn/LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.cupk.learn;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = LearningApplication.class)
+@AutoConfigureMockMvc
+class LearningApplicationTests {
+
+}
